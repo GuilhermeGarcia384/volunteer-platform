0c0c273761d88bffac598b35c0fc6254864599b8
GuilhermeGarcia384 <10409637@mackenzista.com.br>
2025-06-16 17:17:45 -0300

    Ajustes para deployment

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ api = FastAPI()
 # CORS
 api.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=["*"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
